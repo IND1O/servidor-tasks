02845f3c176fa25ae18ae411b2f6389e834e79d7
ind1o <ibuprofenox4@gmail.com>
2023-09-15 17:37:38 -0300

    mejorando el connect de moongo

--- src/db.js
+++ src/db.js
@@ -6,7 +6,10 @@ config();
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DB_MONGO);
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(">>> Base de Datos >Conectada<");
   } catch (error) {
     console.log(error);
